c808086fe97ec524e7a20eb76b7ca9019cf072b9
Yazid <aadnanyazid@hotmail.fr>
2020-01-16 13:43:54 +0100

    Creation Projet Spring Boot
    Projet creer a l'aide de spring boot starter

---
+++ ProjetJ2EE/src/main/java/com/example/ProjetFilRougeGarage/ProjetFilRougeGarageApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjetFilRougeGarage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetFilRougeGarageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetFilRougeGarageApplication.class, args);
+	}
+
+}
