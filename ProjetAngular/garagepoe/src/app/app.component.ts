392b06835ed57e0a6087d44b4f4b4fbc0df969ae
Yazid <aadnanyazid@hotmail.fr>
2020-02-07 10:18:10 +0100

    Creation projet Angular

---
+++ ProjetAngular/garagepoe/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gar-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'garagepoe';
+}
