fc4f2de001b7bca9916914f506b2a1dccd6a63f6
XavierL <34311868+LXavier74@users.noreply.github.com>
2020-02-07 15:25:44 +0100

    Commit lire facture devis pas fini

--- ProjetAngular/garagepoe/src/app/commercial/facture-devis/facture-devis.component.ts
+++ ProjetAngular/garagepoe/src/app/commercial/facture-devis/facture-devis.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FactureDevis, factureDev } from './facture-devis.model';
+import { FactureDevisService } from './facture-devis.service';
 
 @Component({
   selector: 'gar-facture-devis',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./facture-devis.component.css']
 })
 export class FactureDevisComponent implements OnInit {
+  listFactureD: factureDev;
 
-  constructor() { }
+  constructor(private FactureS: FactureDevisService) { }
 
   ngOnInit() {
+    this.listFactureD = this.FactureS.getAll();
+
   }
 
 }
