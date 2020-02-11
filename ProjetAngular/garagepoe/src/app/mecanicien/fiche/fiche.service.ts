e46591b64b83d5e44371bb35d5431fd73d40a094
XavierL <34311868+LXavier74@users.noreply.github.com>
2020-02-11 17:31:31 +0100

    commit fiche delete

--- ProjetAngular/garagepoe/src/app/mecanicien/fiche/fiche.service.ts
+++ ProjetAngular/garagepoe/src/app/mecanicien/fiche/fiche.service.ts
@@ -24,7 +24,7 @@ export class FicheService {
 
    deleteFicheId(id: number) {
     console.log(this.url+ '/'+ id)
-    return this.http.delete(this.url + '/' + id);
+    return this.http.delete(this.url +'/'+ id);
   }
 
   editFicheId(id: number) {
