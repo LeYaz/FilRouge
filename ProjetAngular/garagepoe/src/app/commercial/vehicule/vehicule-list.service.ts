a2498a31c61e65acd777b7b3b3232a6e26123a4e
teddy ruppin <ruppint@gmail.com>
2020-02-11 15:54:48 +0100

    maj edit vehicule

--- ProjetAngular/garagepoe/src/app/commercial/vehicule/vehicule-list.service.ts
+++ ProjetAngular/garagepoe/src/app/commercial/vehicule/vehicule-list.service.ts
@@ -19,11 +19,6 @@ export class VehiculeListService {
      return this.http.get(this.url);
    }
 
-
-   getVehiculesId(id: number) {
-    return this.http.get(this.url + '/' + id);
-  }
-
    deleteVehiculesId(id: number) {
     return this.http.delete(this.url + '/' + id);
   }
@@ -35,44 +30,6 @@ export class VehiculeListService {
   }
 
   get(id:number){
-    return this.http.get(this.url + '/' + id);
+    return this.http.get<Vehicule>(this.url + '/' + id);
   }
-
-
-  // get(id: number) {
-  //   let veh1: Vehicule;
-  //   this.vehiculelist.filter( element => {
-  //     if (element.id === id) {
-  //       veh1 = element;
-  //     }
-  //   });
-  //   return veh1;
-  // }
-
-  // getNewId(){
-  //   let id: number = this.vehiculelist.length + 1;
-  //   return id++;
-  // }
-
-  // add(vehicule: Vehicule){
-  //   vehicule.id = this.getNewId();
-  //   this.vehiculelist.push(vehicule);
-  // }
-
-  // edit(vehicule: Vehicule){
-  //   this.get(vehicule.id).marque = vehicule.marque;
-  //   this.get(vehicule.id).modele = vehicule.modele;
-  //   this.get(vehicule.id).quantite = vehicule.quantite;
-  //   this.get(vehicule.id).prixHT = vehicule.prixHT;
-  //   this.get(vehicule.id).datecreation = vehicule.date_creation;
-  //   this.get(vehicule.id).desactiver = vehicule.desactiver;
-  // }
-
-  // deactive(id: number) {
-  //   for ( let i = 0; i < this.vehiculelist.length; i++) {
-  //     if (this.vehiculelist[i].id == id) {
-  //         this.vehiculelist.splice(i);
-  //     }
-  //   }
-  // }
 }
