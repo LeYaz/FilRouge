de24bb84b4a919ab2b6c5842a82a1fe15309ffa8
Yazid <aadnanyazid@hotmail.fr>
2020-02-13 09:57:15 +0100

    format date

--- ProjetAngular/garagepoe/src/app/mecanicien/mecanicien-routing.module.ts
+++ ProjetAngular/garagepoe/src/app/mecanicien/mecanicien-routing.module.ts
@@ -23,10 +23,8 @@ const routes: Routes = [
     {path:'pieces', component: PiecesComponent},
     {path:'pieces/edit', component:PiecesEditComponent},
     {path:'pieces/edit/:id', component:PiecesEditComponent},
-    {path:'taches', component: TacheComponent}
-
     {path:'taches', component: TacheComponent},
-    {path:'taches/edit/:id', component: EditeTacheComponent},
+    {path:'taches/edit/:id', component: EditeTacheComponent}
 
   ] }
 ];
